786c6d147c89ce218d5c41b9556ad386f3f3c136
Cassie Kaminski <rice.cassie@gmail.com>
2022-06-09 09:18:33 -0700

    edited import statement in index.js

--- frontend/inventory-app/src/index.js
+++ frontend/inventory-app/src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Router, BrowserRouter } from 'react-router-dom';
-import {serverUrl, redirectUri}  from './variables/constants';
+import serverUrl  from './variables/constants';
+import redirectUri from './variables/constants';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
