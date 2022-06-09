103e9421fd517a7b1b2cfa2fb60c2474723b429e
Cassie Kaminski <rice.cassie@gmail.com>
2022-06-09 07:22:03 -0700

    changed export of serverUrl variable

--- frontend/inventory-app/src/variables/constants.js
+++ frontend/inventory-app/src/variables/constants.js
@@ -3,7 +3,4 @@ let prod = '';
 
 let dev = 'http://localhost:5000';
 
-const serverUrl = process.env.NODE_ENV === 'development' ? dev: prod;
-
-
-export default serverUrl;
+export const serverUrl = process.env.NODE_ENV === 'development' ? dev: prod;
