a752f2b4842e32c49adc5ec5350e04ea57c73c38
Cassie Kaminski <rice.cassie@gmail.com>
2022-06-09 07:34:02 -0700

    added React Node env variable

--- frontend/inventory-app/src/variables/constants.js
+++ frontend/inventory-app/src/variables/constants.js
@@ -3,4 +3,4 @@ let prod = '';
 
 let dev = 'http://localhost:5000';
 
-export const serverUrl = process.env.NODE_ENV === 'development' ? dev: prod;
+export const serverUrl = process.env.REACT_APP_NODE_ENV === 'development' ? dev: prod;
