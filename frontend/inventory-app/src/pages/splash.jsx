d1f4e820c3da6a574dae7588d93e9c35c0116ac9
Cassie Kaminski <rice.cassie@gmail.com>
2022-06-16 11:53:09 -0700

    updated splash page to add results img

--- frontend/inventory-app/src/pages/splash.jsx
+++ frontend/inventory-app/src/pages/splash.jsx
@@ -4,6 +4,7 @@ import { Form, Button, Container, Row, Col, Image } from "react-bootstrap";
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
 import surveyImg from '../images/surveyMakerImg.png';
+import surveyResultsImg from '../images/surveyMakerResultsImg.png';
 
 
 export function Splash() {
@@ -19,16 +20,29 @@ export function Splash() {
     <main className='main' style={{ backgroundColor: 'aliceblue'}}>
     <Container  fluid>
       <Row>
-        <Col sm={12} md={3}></Col>
-        <Col sm={12} md={3}  style={{paddingTop: 80, paddingBottom: 50 }}>
+        <Col  xs={12} md={6} lg={6} className="justify-content-md-center"  style={{paddingTop: 80, paddingBottom: 50, textAlign: 'right', paddingRight: 40 }}>
             <h2 style={{fontWeight: 500}}>Get Customer Insights! Increase Sales!</h2><br />
             <h4>Make surveys to get customer insights that will drive future sales!</h4><br />
             <Button onClick={() => loginWithRedirect()} variant="success" style={{fontSize: 20}}>Get Started Now!</Button>
         </Col>
-        <Col sm={12} md={3} style={{paddingTop: 10, paddingBottom: 30, margin: 50, flexGrow: 2 }}>
-            <Image fluid src={surveyImg} className="splashImg"></Image>
+        <Col xs={12}  md={6} lg={6} className="justify-content-md-center" style={{paddingTop: 40, paddingBottom: 50 }}>
+            <div style={{display: 'flex', justifyContent: 'center'}}><img  src={surveyImg} className="splashImg"></img></div>
         </Col>
-        <Col sm={12} md={3}></Col>
+      </Row>
+      <Row className='splashSection'>
+      
+        <Col sm={12} md={12}  style={{paddingTop: 80,}}>
+            <h2 style={{fontWeight: 500, textAlign: 'center'}}>See Your Results in Real Time!</h2>
+        </Col>
+        
+
+      </Row>
+      <Row className='splashSection'>
+      <Col sm={12} md={12} style={{paddingBottom: 30, margin: 10}}>
+            
+            <div style={{display: 'flex', justifyContent: 'center'}}><img style={{maxWidth: "700px"}}  src={surveyResultsImg} className="splashImg"></img></div>
+        </Col>
+
       </Row>
     </Container>
     </main>
